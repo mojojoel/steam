79be003736ab58745136b82f66c4feab3e29f3e5
Joel <apple@MBA.local>
2014-10-30 10:47:07 +0800

    validate price

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ActiveRecord::Base
 	belongs_to :user
+	validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than_or_equal_to => 0.01}
 
 	# def self.approved
 	# 	where(:approved => true)
